268ec1bef707a6476fe9f2c4a85a4e5b8cc67e94
langwanluo <langwnaluo@gmail.com>
2022-08-29 14:31:11 +0800

    jwt token hs256

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/langwan/go-jwt-hs256
+
+go 1.19
---
+++ jwt_test.go
@@ -0,0 +1,21 @@
+package go_jwt_hs256
+
+import "testing"
+
+func init() {
+	Secret = "123456"
+}
+
+func TestSign(t *testing.T) {
+	token, _ := Sign(struct {
+		Name string `json:"name"`
+	}{Name: "langwan"})
+	t.Log(token)
+
+	err := Verify(token)
+	if err != nil {
+		t.Error("failed")
+	} else {
+		t.Log("ok")
+	}
+}
